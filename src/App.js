86bbe5fa7e7b070c824b87f8a79dfa31ad71a7cf
VeikkQ <53118042+VeikkQ@users.noreply.github.com>
2022-10-23 19:10:11 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -7,12 +7,13 @@ function App() {
 
     const [lat, setLat] = useState(0);
     const [lng, setLng] = useState(0);
-
+    const [isLoading, setIsLoading] = useState(true);
     useEffect(() => {
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition((position) => {
           setLat(position.coords.latitude);
           setLng(position.coords.longitude);
+          setIsLoading(false);
         }, (error) => {
           alert(error);
         })
@@ -21,18 +22,30 @@ function App() {
         alert('Your browser dos not support geolocation!');
       }
     }, []);
-  return (
-    <div className='content'>
-      <h3>Your position</h3>
-      <p>
-        Position:&nbsp;
-        {lat.toFixed(3)},
-        {lng.toFixed(3)}
+  
+    if (isLoading) {
+      return (
+       <P>Loading....</P>
+      )
+    }
+      
+     else {
+    return (
+        <div className='content'>
+          <h3>Your position</h3>
+          <p>
+            Position:&nbsp;
+            {lat.toFixed(3)},
+            {lng.toFixed(3)}
+    
+          </p>
+          <Weather lat={lat} lng={lng} />
+        </div>
+      ); 
+    }
+    }
+  
+    
 
-      </p>
-      <Weather lat={lat} lng={lng} />
-    </div>
-  ); 
-}
 
 export default App;
